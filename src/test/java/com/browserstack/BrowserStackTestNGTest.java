6a47b27463e89da66df2f6822ec5fdfe625c760f
Varun Garg <varun.ga@browserstack.com>
2019-02-15 16:57:44 +0530

    java 10 and selenium 3
    Signed-off-by: Varun Garg <varun.ga@browserstack.com>

--- src/test/java/com/browserstack/BrowserStackTestNGTest.java
+++ src/test/java/com/browserstack/BrowserStackTestNGTest.java
@@ -1,31 +1,20 @@
 package com.browserstack;
-import com.browserstack.local.Local;
 
-import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Iterator;
+import java.util.Map;
+
+import com.browserstack.local.Local;
+
 import org.json.simple.JSONObject;
-import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
-
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Test;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Factory;
-import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 
 
 public class BrowserStackTestNGTest {
