e9786bf9f1583db92777ee5f5824e39c988b222a
papa <michael.kirpa@gmail.com>
2020-04-03 19:57:34 +0300

    add prod variables add prod travis run

--- src/test/java/com/paysuper/tests/TestBase.java
+++ src/test/java/com/paysuper/tests/TestBase.java
@@ -3,9 +3,6 @@ package com.paysuper.tests;
 import com.paysuper.appmanager.ApplicationManager;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import io.restassured.RestAssured.*;
-import io.restassured.matcher.RestAssuredMatchers.*;
-import org.hamcrest.Matchers.*;
 
 public class TestBase {
 
