ec3e9feb272b4f7cc8536b90064b335e9b40982b
papa <michael.kirpa@gmail.com>
2020-05-21 16:45:21 +0300

    fix select language

--- src/test/java/com/paysuper/appmanager/pages/payform/PayFormPage.java
+++ src/test/java/com/paysuper/appmanager/pages/payform/PayFormPage.java
@@ -25,8 +25,7 @@ public class PayFormPage extends AbstractPage {
 
     public void selectLanguage(String lang){
         WebElement hoverElement = driver.findElement(Locators.get("DemoFormPage.languageSelectField"));
-        System.out.println(hoverElement.getText());
-        if(hoverElement.getText() != lang){
+        if(hoverElement.getText().equals(lang)){
             Actions action = new Actions(driver);
             action.moveToElement(hoverElement).perform();
             driver.findElement(By.xpath("//div[contains(text(),'"+lang+"')]")).click();
