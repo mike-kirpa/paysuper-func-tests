f2386956631605817bdbb9a7334557e77cdb223a
papa <michael.kirpa@gmail.com>
2020-07-09 17:14:24 +0300

    add mail client

--- src/test/java/com/paysuper/appmanager/helpers/MailParser.java
+++ src/test/java/com/paysuper/appmanager/helpers/MailParser.java
@@ -1,24 +1,22 @@
 package com.paysuper.appmanager.helpers;
 
-import org.jsoup.Jsoup;
+import org.apache.commons.mail.util.MimeMessageParser;
 
 import java.io.*;
 import java.util.*;
-import javax.activation.UnsupportedDataTypeException;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMultipart;
-import javax.mail.search.AndTerm;
+import javax.mail.internet.MimeMessage;
 import javax.mail.search.FromTerm;
 import javax.mail.search.SearchTerm;
 import javax.mail.search.SubjectTerm;
 import java.io.IOException;
-import org.jsoup.nodes.Element;
+
 
 public class MailParser {
     public MailParser() {
     }
-    public static Object getMail() throws MessagingException, IOException {
+    public static Object getMail() throws IOException {
         Folder folder = null;
         Store store = null;
         Object content = null;
@@ -34,8 +32,7 @@ public class MailParser {
             Session session = Session.getDefaultInstance(props, null);
             // session.setDebug(true);
             store = session.getStore("imaps");
-          System.out.println(properties.getProperty("email"));
-            store.connect("imap.gmail.com", properties.getProperty("email"), properties.getProperty("password"));
+            store.connect("imap.gmail.com", "autotest.protocolone@gmail.com", System.getenv("autotest_email_pass"));
             folder = store.getFolder("Inbox");
             /* Others GMail folders :
              * [Gmail]/All Mail   This folder contains all of your Gmail messages.
@@ -45,65 +42,29 @@ public class MailParser {
              * [Gmail]/Starred    Starred messages.
              * [Gmail]/Trash      Messages deleted from Gmail.
              */
-            folder.open(Folder.READ_WRITE);
-            SearchTerm sender = new FromTerm(new InternetAddress(properties.getProperty("internetAddress")));
+            folder.open(Folder.READ_ONLY);
+            SearchTerm sender = new FromTerm(new InternetAddress("googlecommunityteam-noreply@google.com"));
             SearchTerm subject = new SubjectTerm(properties.getProperty("subjectTerm"));
-            Message[] messages = folder.search(new AndTerm(sender, subject));
-            //  Message[] messages = folder.search(sender);
-
+            //Message[] messages = folder.search(new AndTerm(sender, subject));
+            Message[] messages = folder.search(sender);
 
             for (int i = 0; i < messages.length; ++i) {
-                System.out.println("MESSAGE #" + (i + 1) + ":");
-                Message msg = messages[i];
-                content = msg.getContent();
-/*
-          if we don''t want to fetch messages already processed
-          if (!msg.isSet(Flags.Flag.SEEN)) {
-             String from = "unknown";
-             ...
-          }
-        */
-
+                MimeMessage msg = (MimeMessage) messages[i];
+                content = readHtmlContent(msg);
             }
-        } finally {
-            if (folder != null) {
-                folder.close(true);
-            }
-            if (store != null) {
-                store.close();
-            }
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
         return content;
     }
 
-    public static String getContent(Object content) throws MessagingException, IOException {
-
-        String body = null;
-        //   Object content = msg.getContent();
-        if (content instanceof MimeMultipart) {
-            MimeMultipart multipart = (MimeMultipart) content;
-            if (multipart.getCount() > 0) {
-                BodyPart part = multipart.getBodyPart(0);
-                content = part.getContent();
-                body = content.toString();
-            }
-        } else if (content instanceof String) {
-            body = (String) content;
-        } else
-            throw new UnsupportedDataTypeException();
-
-
-        return body;
+    public static String readHtmlContent(MimeMessage message) throws Exception {
+        return new MimeMessageParser(message).parse().getHtmlContent();
     }
-
-//    public static void main(String args[]) throws Exception {
-//
-//        //  ReceiveMailImap.getContent(getMail());
-//        String HTMLSTring = MailParser.getContent(getMail());
-//        org.jsoup.nodes.Document html = Jsoup.parse(HTMLSTring);
-//        Element link = html.select("a").first();
-//        String relHref = link.attr("href"); // == "/"
-//        System.out.println(relHref);
-//    }
 }
--- src/test/java/com/paysuper/tests/SingleTest.java
+++ src/test/java/com/paysuper/tests/SingleTest.java
@@ -1,18 +1,25 @@
 package com.paysuper.tests;
 
+import com.paysuper.appmanager.helpers.MailParser;
 import com.paysuper.appmanager.pages.PayFormPage;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
 import org.testng.annotations.Test;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+
+import static com.paysuper.appmanager.helpers.MailParser.getMail;
 
 
 public class SingleTest extends TestBase {
 
     @Test()
     public void test() throws Exception {
-        String payment_form_url = app.restAPI.createOrder(
-                app.getProperties.value("projectid"),
-                Integer.valueOf(app.getProperties.value("amount")),
-                app.getProperties.value("currency")).jsonPath().get("payment_form_url");
-        app.driver.get(payment_form_url);
-        PayFormPage payFormPage =new PayFormPage(app.driver);
+        String HTMLSTring = (String) MailParser.getMail();
+        org.jsoup.nodes.Document html = Jsoup.parse(HTMLSTring);
+        Element link = html.selectFirst("a:contains(Аккаунт Google)");
+        String relHref = link.attr("href"); // == "/"
+        System.out.println(relHref);
     }
 }
