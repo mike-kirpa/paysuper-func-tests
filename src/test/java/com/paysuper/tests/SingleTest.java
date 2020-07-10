2846f42bcc5510d6f28e01ad07852f2de7813511
Михаил Кирпа <mikhail.kirpa@gamenet.ru>
2020-07-10 10:59:12 +0300

    cleaned import libs

--- src/test/java/com/paysuper/tests/SingleTest.java
+++ src/test/java/com/paysuper/tests/SingleTest.java
@@ -1,22 +1,9 @@
 package com.paysuper.tests;
 
-
 import com.paysuper.appmanager.helpers.MailParser;
-import com.paysuper.appmanager.pages.PayFormPage;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
-import com.paysuper.appmanager.pages.analytics.AnalyticsLogin;
-import com.paysuper.appmanager.pages.dashboard.DashboardLoginPage;
-import com.paysuper.appmanager.pages.dashboard.DashboardTransactionsPage;
-import com.paysuper.appmanager.pages.payform.PayFormPage;
-import org.openqa.selenium.By;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-import javax.mail.MessagingException;
-import java.io.IOException;
-
-import static com.paysuper.appmanager.helpers.MailParser.getMail;
-
 
 public class SingleTest extends TestBase {
 
