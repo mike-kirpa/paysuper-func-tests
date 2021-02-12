bcb2dd87655a4bbf76fb0fe2837a4b23a7299d04
papa <michael.kirpa@gmail.com>
2021-02-12 11:49:04 +0200

    the response is taken out of the method

--- src/test/java/com/paysuper/appmanager/helpers/RestAPI.java
+++ src/test/java/com/paysuper/appmanager/helpers/RestAPI.java
@@ -15,12 +15,10 @@ import static io.restassured.RestAssured.*;
 
 
 public class RestAPI {
-
+        private Response response;
 
 
     public String createSimpleOrder(String apiURL, Order order) {
-        Response response;
-
         RestAssured.baseURI = apiURL;
         RequestSpecification request = given();
         JSONObject requestParams = new JSONObject();
@@ -41,9 +39,7 @@ public class RestAPI {
         List<String> list = new ArrayList<String>();
         list.add(order.getProduct());
         JSONArray array = new JSONArray();
-        for (int i = 0; i < list.size(); i++) {
-            array.add(list.get(i));
-        }
+        array.addAll(list);
         RequestSpecification request = given();
         JSONObject requestParams = new JSONObject();
         requestParams.put("project", order.getProjectId());
@@ -57,16 +53,13 @@ public class RestAPI {
     public String createTokenOrder(String apiURL, Order order, Email email) {
 
         String payload = "{\n    \"user\": {\n        \"id\": \"0000000000003\",\n        \"email\": {\n            \"value\": \"" + email.getEmailRecipient() + "\",\n            \"verified\": true\n        },\n        \"phone\": {\n            \"value\": \"0123456\",\n            \"verified\": true\n        },\n        \"name\": {\n            \"value\": \"Человек Х -токен\"\n        },\n        \"ip\": {\n            \"value\": \"194.54.160.79\"\n        },\n        \"locale\": {\n            \"value\": \"de-DE\"\n        },\n        \"address\": {\n            \"country\": \"DE\",\n            \"city\": \"Odessa\",\n            \"state\": \"Odessa\",\n            \"postal_code\": \"a300\"\n        },\n        \"metadata\": {\n            \"status\": \"VIP\"\n        }\n    },\n    \"settings\": {\n        \"project_id\": \""+order.getProjectId()+"\",\n        \"currency\": \"EUR\",\n        \"amount\": 100,\n        \"description\": \"Какое-то описание платежа\",\n        \"metadata\": {\n            \"balance\": \"999.99\"\n        },\n        \"type\": \"simple\"\n    }\n}";
-        return
-                given()
+        response = given()
                         .header("X-API-SIGNATURE", order.getSecret())
                         .header("Content-Type", "application/json")
                         .body(payload)
-                        .post(apiURL+"/tokens")
-                        .then()
-                        .statusCode(200)
-                        .extract()
-                        .path("payment_form_url");
+                        .post(apiURL+"/tokens");
+        order.setUUID((String) response.then().extract().path("id"));
+        return response.then().extract().path("payment_form_url");
     }
 
     public void getOrderForPayForm(String apiURL, Order order){
