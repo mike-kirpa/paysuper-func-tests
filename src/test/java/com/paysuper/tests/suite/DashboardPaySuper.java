2462ad82262fffd94bf063855384048e170a3825
papa <michael.kirpa@gmail.com>
2021-02-12 11:52:17 +0200

    removed unnecessary imports

--- src/test/java/com/paysuper/tests/suite/DashboardPaySuper.java
+++ src/test/java/com/paysuper/tests/suite/DashboardPaySuper.java
@@ -1,22 +1,14 @@
 package com.paysuper.tests.suite;
 
-import com.paysuper.appmanager.helpers.MailParser;
-import com.paysuper.appmanager.models.Email;
-import com.paysuper.appmanager.models.Order;
+
 import com.paysuper.appmanager.pages.dashboard.DashboardLoginPage;
-
 import com.paysuper.appmanager.pages.dashboard.DashboardMainPage;
-import com.paysuper.appmanager.pages.dashboard.DashboardTransactionsPage;
-import com.paysuper.appmanager.pages.dashboard.OrderPage;
-import org.openqa.selenium.By;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import com.paysuper.tests.TestBase;
 
 public class DashboardPaySuper extends TestBase {
 
-    @Test(enabled = true, description="Dashboard Login-Logout",
+    @Test(description="Dashboard Login-Logout",
             groups = {"tst", "stg", "prod"})
     public void LoginSuccess() {
         app.driver.get(app.getProperties.value("DashboardUrl"));
