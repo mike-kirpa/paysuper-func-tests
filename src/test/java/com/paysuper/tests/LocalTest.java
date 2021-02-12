afd50228dd325b6679a833de52612ee3a9ae25a8
papa <michael.kirpa@gmail.com>
2021-02-12 11:53:42 +0200

    removed unnecessary imports

--- src/test/java/com/paysuper/tests/LocalTest.java
+++ src/test/java/com/paysuper/tests/LocalTest.java
@@ -1,15 +1,14 @@
 package com.paysuper.tests;
 
-import com.paysuper.appmanager.pages.dashboard.*;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LocalTest extends TestBase {
 
     @Test
-    public void test2() throws Exception {
+    public void test2() {
         app.driver.get("http://bs-local.com:45691/check");
-
         Assert.assertTrue(app.driver.getPageSource().contains("Up and running"));
     }
 }
