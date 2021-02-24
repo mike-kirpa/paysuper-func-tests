0f6cacbe6f86befb73ee59a48e098f7d3938dfe6
Mikhail Kyrpa <michael.kirpa@gmail.com>
2021-02-24 13:38:51 +0200

    added static method

--- src/test/java/com/paysuper/appmanager/helpers/GetProperties.java
+++ src/test/java/com/paysuper/appmanager/helpers/GetProperties.java
@@ -1,26 +1,33 @@
 package com.paysuper.appmanager.helpers;
 
+import com.paysuper.appmanager.ApplicationManager;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
 public class GetProperties {
-    String filename;
-    //файл, который хранит свойства нашего проекта
-    public Properties properties = new Properties();
 
-    public GetProperties(String filename)  {
-        File file = new File("src/test/resources/EnvironmentVariables/" + filename +".properties");
-        this.filename = filename;
+    //файл, который хранит свойства нашего проекта
+    public static Properties properties = new Properties();
+/*
+    public GetProperties()  {
+        File file = new File("src/test/resources/EnvironmentVariables/" + ApplicationManager.zone +".properties");
         try {
             properties.load(new FileReader(file));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    public String value (String key){
+*/
+    public static String value(String key){
+        File file = new File("src/test/resources/EnvironmentVariables/" + ApplicationManager.zone +".properties");
+        try {
+            properties.load(new FileReader(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return properties.getProperty(key);
     }
 }
